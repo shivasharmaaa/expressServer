7b143b7b97cf94b4acbbc2cae995c8c4509ff702
shivasharmaaa <shivasharma99495@gmail.com>
2025-10-02 12:02:55 +0530

    "added login middleware"

--- App.js
+++ App.js
@@ -4,10 +4,25 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/welcome/:username", (req, res, next) => {
-  const name = req.params.username;
-  const role = req.query.role;
-  res.send(`welcome  ${name} your role is ${role}`);
+app.use((req, res, next) => {
+  console.log(`method is ${req.method} and url is ${req.url}`);
+  next();
+});
+
+app.get("/products", (req, res) => {
+  res.send("Here is the list of all products");
+});
+
+app.post("/products", (req, res) => {
+  res.send("A new product has been added");
+});
+
+app.get("/categories", (req, res) => {
+  res.send("Here is the list of all categories");
+});
+
+app.post("/categories", (req, res) => {
+  res.send("A new category has been created");
 });
 
 app.listen(4000, () => {
